c03a8f7a408b081de3f30cf7acd6a39707bfe7ab
Simon-Coolguy <simon.gidalevitz@gmail.com>
2023-01-20 21:47:18 -0600

    Finished project euler # 104

--- eulerproj-104.py
+++ eulerproj-104.py
@@ -1,9 +1,5 @@
 fibonacci = [1,1]
 
-def gen_fibonacci():
-    global fibonacci
-    fibonacci.append(fibonacci[0] + fibonacci[1])
-    return fibonacci[1]
 def pcbeg(n):
     check = []
     through19 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -12,39 +8,35 @@ def pcbeg(n):
             check.append(int(str(n)[x-1]))
         checks = all(item in check for item in through19)
     
-        if checks is True:
-            return True    
-        else :
-            return False
-    
-
-def pcend(n):
-    check = []
-    through19 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    if len(str(n)) > 10:
-        for x in range(1,11):
-            check.append(int(str(n)[-x]))
-        checks = all(item in check for item in through19)
-    
-        if checks is True:
-            return True    
-        else :
+        if checks is True:   
+            check = []
+            through19 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            for x in range(1,11):
+                check.append(int(str(n)[-x]))
+            checks = all(item in check for item in through19)
+        
+            if checks is True:
+                return True    
+            else :
+                return False
+        else:
             return False
 
-fib = 0
+
 def run_test():
-    global fib
+    global fibonacci
     i = 0
     a = 50
     while i == 0:
-        c = gen_fibonacci() 
-        if pcbeg(c) and pcend(c):
-            i = 1
+        fibonacci.append(fibonacci[0] + fibonacci[1])
+        c = fibonacci[2]
+        fibonacci.pop(0)
+        if a > 329467:
+            if pcbeg(c):
+                i = 1
         a += 1
-        if a % 1000000 == 0:
+        if a % 100 == 0:
             print(a)
-            print(c)
-            print(fibonacci)
-    print(gen_fibonacci(a-1))
+    print(c)
 
 run_test()
