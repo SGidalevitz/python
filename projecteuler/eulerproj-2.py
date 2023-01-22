4d6fae907acc48c74604a393353f71065da4b8dd
Simon-Coolguy <simon.gidalevitz@gmail.com>
2023-01-22 11:26:35 -0600

    this might already be on here :skull:

---
+++ projecteuler/eulerproj-2.py
@@ -0,0 +1,9 @@
+fnumbers = [1, 2]
+while fnumbers[-1] <= 4000000:
+    fnumbers.append(fnumbers[-1] + fnumbers[-2])
+y = 0
+for x in fnumbers:
+    if x % 2 != 0:
+        fnumbers.remove(x)
+    y = y + x
+print(y)
