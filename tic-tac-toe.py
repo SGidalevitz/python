f41a2b446324284f79a1bbb5ceb19cac61fe1712
Simon-Coolguy <simon.gidalevitz@gmail.com>
2023-01-22 15:49:02 -0600

    Added colors and strikethrough

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -5,8 +5,17 @@ board = [
     [0, 0, 0], 
     [0, 0, 0]]
 class style:
-   BOLD = '\033[1m'
-   END = '\033[0m'
+    BOLD = "\033[1m"
+    END = "\033[0m"
+    BLACK = "\033[30m"
+    RED =  "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    STRIKETHROUGH = "\033[9m"
 
 
 TTT_EMPTY = 0
@@ -38,6 +47,7 @@ class Player:
                 print("This tile is already taken.")
             
 class Computer(Player):
+    color = style.BLUE
     def get_move_pos(self):
         moves = []
         randmoves = []
@@ -50,6 +60,7 @@ class Computer(Player):
     
     
 class Human(Player):
+    color = style.RED
     def get_move_pos(self):
         valid = False
         while not valid:
@@ -73,36 +84,42 @@ class Human(Player):
 player = Human(TTT_X)
 computer = Computer(TTT_O)
 
-def print_board():
+def print_board(player):
+    
+        
+        
     for i in range(3):
         for x in range(3):
+            prefix = ""
+            if (i, x) in winseq:
+                prefix = style.STRIKETHROUGH + player.color
+            elif (board[i], board[x]) in recents:
+                prefix = style.BOLD
             if board[i][x] == 0:
-                print(".", end="")
+                print(prefix  + "." + style.END, end="")
             if board[i][x] == 1:
-                if (board[i],board[x]) in recents:
-                    print(style.BOLD + "X" + style.END, end="")
-                else:
-                    print("X", end = "")
+                print(prefix + style.RED + "X" + style.END, end = "")
             if board[i][x] == 2:
-                if (board[i],board[x]) in recents:
-                    print(style.BOLD + "O" + style.END, end="")
-                else:
-                    print("O", end = "")
-            print(" ", end="")
+                print(prefix + style.BLUE + "O" + style.END, end = "")
+            if x != 3:
+                print(prefix + " " + style.END, end="")
         print()
+
 
 def run_game():
   while True:
     if wincheck(TTT_O):
         print("You lose.")
+        print_board(Computer)
         exit()
     elif player.totalturns > 8:
         print("Draw.")
     player.turn()
-    print_board()
+    print_board(None)
     player.totalturns += 1
     if wincheck(TTT_X):
         print("Congratulations! You Win!")
+        print_board(Human)
         exit()
     elif player.totalturns > 8:
         print("Draw.")
@@ -110,25 +127,36 @@ def run_game():
     time.sleep(0.5)
     print("_____")
     computer.turn()
-    print_board()
+    print_board(None)
     player.totalturns += 1
     
 
+winseq = []
 def wincheck(symbol):
     if wincheck_row(symbol) or wincheck_column(symbol) or wincheck_diag_1(symbol) or wincheck_diag_2(symbol):
         return True
 def wincheck_row(symbol):
+    global winseq
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] and board[i][0] == symbol:
+            winseq = [(i, 0), (i, 1), (i, 2)]
             return True
 def wincheck_column(symbol):
+    global winseq
     for i in range(3):    
         if board[0][i] == board[1][i] == board[2][i] and board[0][i] == symbol:
+            winseq = [(0, i), (1, i), (2, i)]
             return True
 def wincheck_diag_1(symbol):
-    return board[0][0] == board[1][1] == board[2][2] and board[0][0] == symbol
+    global winseq
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] == symbol:
+        winseq = [(0, 0), (1, 1), (2, 2)]
+        return True
 def wincheck_diag_2(symbol):
-    return board[2][0] == board[1][1] == board[0][2] and board[2][0]== symbol
+    global winseq
+    if board[2][0] == board[1][1] == board[0][2] and board[2][0]== symbol:
+        winseq = [(2, 0), (1, 1), (0, 2)]
+        return True
 
 
 
