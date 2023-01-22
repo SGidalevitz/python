df8217f57bd9ebbdd60398d5f01ce6812f09ee5a
Simon-Coolguy <simon.gidalevitz@gmail.com>
2023-01-21 21:26:30 -0600

    new features: wincheck and input check

--- tic-toe-tac.py
+++ tic-toe-tac.py
@@ -3,43 +3,110 @@ board = [
     [0, 0, 0], 
     [0, 0, 0], 
     [0, 0, 0]]
-a = board[0]
-b = board[1]
-c = board[2]
-player = random.randrange(1,3)
-computer = (3 - player)
-def game():
-    turn(player)
-    computer()
+
+TTT_EMPTY = 0
+TTT_X = 1
+TTT_O = 2
+
 def place(xy, row, col):
-    board[row][col] = xy
+    if board[row][col] == 0:
+        board[row][col] = xy
+        return True
+    else:
+        return False
+class Player:
+    symbol = TTT_EMPTY
+    def __init__(self, sym):
+        self.symbol = sym
+    def turn(self):
+        while True:
+            pos = self.get_move_pos()
+            if place(self.symbol, pos[0], pos[1]):
+                break
+            else:
+                print("This tile is already taken.")
+            
+class Computer(Player):
+    def get_move_pos(self):
+        moves = []
+        randmoves = []
+        for x in range(3):
+            for y in range(3):
+                if board[x][y] == 0:
+                    moves.append((x, y))
+        randmoves = moves[random.randrange(len(moves)-1)]
+        return randmoves
+    
+    
+class Human(Player):
+    def get_move_pos(self):
+        valid = False
+        while not valid:
+            move_str = input("Where would you like to move? ")
+            if move_str == "ex":
+                exit()
+            pos = move_str.split(",")
+            
+            if len(pos) != 2:
+                print("Please enter a valid position.")
+                continue
+            for element in pos:
+                if element == "1" or element == "2" or element == "3":
+                    valid = True
+                else:
+                    print("Please enter a valid position.")
+                    break
+        for i in range(2):
+            pos[i] = int(pos[i])-1
+        return pos    
+player = Human(TTT_X)
+computer = Computer(TTT_O)
 
-def turn(xy):
-    move = input("Where would you like to move?")
-    rowcol = move.split(",")
-    for x in rowcol:
-        rowcol[rowcol.index(x)] = int(x)
-    place(xy, rowcol[0], rowcol[1])
-def computer():
-    moves = []
-    for x in range(3):
-        for y in range(3):
-            if board[x][y] == 0:
-                moves.append((x, y))
-    print(moves)
-    #place(computer, moves[random.randrange(0,8)])
-    #place(computer, )
+def print_board():
+    for i in range(3):
+        for x in range(3):
+            if board[i][x] == 0:
+                print(".", end="")
+            if board[i][x] == 1:
+                print("X", end="")
+            if board[i][x] == 2:
+                print("Y", end="")
+            print(" ", end="")
+        print()
+
+def run_game():
+  while True:
+    if wincheck():
+        print("Congratulations! You Win!")
+        print_board()
+        exit()
+    player.turn()
+    if wincheck():
+        print("Congratulations! You Win!")
+        print_board()
+        exit()
+    computer.turn()
+    print_board()
+    
+
+def wincheck():
+    if wincheck_row() or wincheck_column() or wincheck_diag_1() or wincheck_diag_2():
+        return True
+def wincheck_row():
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] > 0:
+            return True
+def wincheck_column():
+    for i in range(3):    
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] > 0:
+            return True
+def wincheck_diag_1():
+    return board[0][0] == board[1][1] == board[2][2] and board[0][0] > 0
+def wincheck_diag_2():
+    return board[2][0] == board[1][1] == board[0][2] and board[2][0] > 0
 
 
-turn(1)
-computer()
 
 
 
-
-print(a)
-print(b)
-print(c)
-#print("\u0332|".join((a)))
-#print("\u0332|".join((b)))
-#print("\u0332|".join((c)))
+run_game()
