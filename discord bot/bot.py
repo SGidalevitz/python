03c6c94cb8c2ddd408cdbe49e838c7a9b31df299
Simon-Coolguy <simon.gidalevitz@gmail.com>
2023-02-06 20:47:23 -0600

    Bot progress- added logs and ton of other stuff

--- discord bot/bot.py
+++ discord bot/bot.py
@@ -1,15 +1,28 @@
 import discord
 
 import responses
-
+import datetime
 
 
 async def send_message(message, user_message, is_private):
     try:
         response = responses.get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        if is_private:
+            await message.author.reply(response) 
+        else:
+            if message.channel.send(response) != 'I didn\'t understand what you wrote. Try typing "!help".':
+                await message.reply(response)
     except Exception as e:
         print(e)
+async def channel_send(message, user_message, channel_id):
+        await embed(user_message, channel_id, 0x00ff00)
+async def embed(user_message, channel_id, color):
+    embedVar = discord.Embed(title=user_message, color=color)
+    await channel_id.send(embed=embedVar)
+    
+
+
+
 
 def run_discord_bot():
     TOKEN = ""
@@ -31,9 +44,13 @@ def run_discord_bot():
         username = str(message.author)
         user_message = str(message.content)
         channel = str(message.channel)
-
-        print(f'{username} said: "{user_message}" ({channel})')
-
+        user_id = str(message.author.id)
+        channel_id = client.get_channel(1072313084267462746)
+        timestamp = datetime.datetime.now().strftime("%I:%M:%S %p, on %B %d")
+        await channel_send(message, (f'{username} ({user_id}) said: "{user_message}" in #{channel} at {timestamp})'), channel_id)
+        
+        
+        
         if user_message[0] == '?':
             user_message = user_message[1:]
             await send_message(message, user_message, is_private=True)
@@ -41,8 +58,7 @@ def run_discord_bot():
             await send_message(message, user_message, is_private=False)
     @client.event
     async def on_member_join(member):
-        send_message('Hello, welcome to the server!')
-
+        send_message('filler', 'Hello, welcome to the server!', is_private=True)
     client.run(TOKEN)
 
 run_discord_bot()
--- discord bot/responses.py
+++ discord bot/responses.py
@@ -2,7 +2,10 @@ import random
 import openai
 from googletrans import Translator
 import discord
-openai.api_key = "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"
+openai.api_key = ""
+with open("openaiapi.key", "r") as f:
+    openai.api_key = f.read()
+
 
 def translate(dest_language, text):
     translator = Translator(service_urls=['translate.google.com'])
@@ -13,32 +16,30 @@ def translate(dest_language, text):
 # privet russian
 def get_response(message: str) -> str:
     p_message = message.lower()
-
-    if p_message == 'hello':
-        return 'Hey it works!'
-    if p_message == '!flip':
-        return "heads" if random.randint(1,2) == 1 else "tails"
-    if p_message == 'roll':
-        return str(random.randint(1, 6))
-    if p_message.startswith('!translate'):
-        language = message.split(' ')[1]
-        return translate(language, message[len("!translate") + len(language):])
-    if p_message == '!help':
-        return "`This is a help message that you can modify.`"
-    if p_message.startswith('!math'):
-        return int(p_message[5:])
-    if p_message == '!restart':
-        import bot
-        exit()
-    if p_message == '!kill':
-        exit()
-    if p_message.startswith("cgpt"):
-        completion = openai.Completion.create(engine="davinci", prompt=p_message[5:], temperature = 0.4)
-        return completion.choices[0].text
-    if p_message == '!guild':
-        return 'hello'
-        #discord.get_guild()
-    
-    
-    return 'I didn\'t understand what you wrote. Try typing "!help".'
+    if p_message.startswith('!'):
+        if p_message == '!hello':
+            return 'Hey it works!'
+        if p_message == '!flip':
+            return "heads" if random.randint(1,2) == 1 else "tails"
+        if p_message == '!roll':
+            return str(random.randint(1, 6))
+        if p_message.startswith('!translate'):
+            language = message.split(' ')[1]
+            return translate(language, message[len("!translate") + len(language):])
+        if p_message == '!help':
+            return ""
+        if p_message.startswith('!math'):
+            return int(p_message[5:])
+        if p_message == '!restart':
+            import bot
+            exit()
+        if p_message == '!kill':
+            exit()
+        if p_message.startswith("!cgpt"):
+            completion = openai.Completion.create(engine="text-davinci-003", prompt=p_message[5:], temperature = 0)
+            return completion.choices[0].text
+        if p_message == '!guild':
+            return 'hello'
+            #discord.get_guild()
+        return 'I didn\'t understand what you wrote. Try typing "!help".'
     
